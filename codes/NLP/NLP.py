da70b5501b790b17a11d223f0e85751069b6b4c5
Louise <louise.stoschek@esme.fr>
2020-06-25 16:52:49 +0200

    NLP VSCode

--- codes/NLP/NLP.py
+++ codes/NLP/NLP.py
@@ -28,32 +28,23 @@ for n in range(len(useless_words)):
     useless_words[n] = useless_words[n].rstrip('\n')
     
 # Union on all 3 lists "stop_words".
-STOP_WORDS = nltk_stopwords.union(sklearn_stopwords)
-#STOP_WORDS = nltk_stopwords.union(sklearn_stopwords).union(useless_words)
+STOP_WORDS = nltk_stopwords.union(sklearn_stopwords).union(useless_words)
 
-#print(STOP_WORDS)
 
 # Load data that contains series storylines.
 df_storylines = pd.read_csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé2_2019-2020\\S2\\UE1\\DataTools\\Projet\series_storylines.csv",
                       header=0, index_col=0)
+#type(df_storylines)
 
-type(df_storylines)
+# Create dataframe with top 10 series.
+df_top_ten = pd.DataFrame(df_storylines.iloc[0:10,0])
 
+
+"""Clean Data"""
 
 # Convert all text to lowercase.
 df_storylines["Storyline"] = df_storylines["Storyline"].str.lower()
 
-liste=[]
-for n in range(len(df_storylines)):
-    
-    words_storyline = df_storylines.iloc[n, 0].split() # Chaque mot de la liste
-    
-    # Remove punctuation from a string.
-    remove_punctuation = str.maketrans("","", string.punctuation)
-    for n in range(len(words_storyline)):
-        words_storyline[n] = words_storyline[n].translate(remove_punctuation)
-        liste.append(words_storyline)
-type(liste)
 
 def remove_punctuation(story):
     
@@ -95,40 +86,52 @@ df_storylines["Storyline"] = df_storylines["Storyline"].apply(remove_punctuation
 df_storylines["Storyline"] = df_storylines["Storyline"].apply(remove_digits)
 
 
-cv = CountVectorizer(stop_words = STOP_WORDS, ngram_range=(1,1), max_features=500)
+"""Analyse Data : countVectorizer"""
 
-Xtrain, x_test, y_train, y_test = train_test_split(df_storylines, test_size=0.2)
-df_storyline_train.shape()
+cv = CountVectorizer(stop_words = STOP_WORDS, ngram_range=(1,1), max_features=2000)
 
 # Apply CV on train data
-
 cv.fit(df_storylines["Storyline"])
-
-train_cv = cv.transform(df_storylines_train["Storyline"])
-test_cv = cv.transform(df_storylines_test["Storyline"])
-
-new_df_storylines = pd.DataFrame(train_cv.toarray(), columns=cv.get_feature_names())
-
-new_df_storylines = new_df_storylines.drop('series', axis=1)
-new_df_storylines.head(1)
+cv_transform = cv.transform(df_storylines["Storyline"])
+new_df_storylines = pd.DataFrame(cv_transform.toarray(), columns=cv.get_feature_names())
 
 word_frequency = new_df_storylines.sum(axis=0).sort_values(ascending=False)
-word_frequency
 
 plt.figure(figsize=(30, 60))
-word_frequency[:300].plot.barh()
-plt.title("Distribution of Word frequency (Top 300)")
+word_frequency[:100].plot.barh()
+plt.title("Distribution of Word frequency (Top 100)")
 plt.xlabel("Words")
 plt.ylabel("Frequency")
 #plt.show()
 
 
+"""Analyse Data : TF-IDF"""
 
+from  sklearn.feature_extraction.text import TfidfVectorizer
 
+tfidf = TfidfVectorizer(lowercase=True, 
+                         stop_words=STOP_WORDS, 
+                         ngram_range=(1,1), 
+                         max_features=2000)
+# Transform list of documents to matrice : fréquency
+csr_mat = tfidf.fit_transform(df_top_ten["Storyline"])
+#csr_mat.toarray()
+words = tfidf.get_feature_names()
+df_tfidf = pd.DataFrame(index=df_top_ten["Storyline"], data=csr_mat.toarray(), columns=words)
 
+#Print usefull words : not high frequency, bring a difference
+inverse_document_freq = pd.DataFrame({"idf":tfidf.idf_}, 
+                                     index=tfidf.get_feature_names())
+inverse_document_freq = inverse_document_freq.sort_values(by="idf", ascending=False)
 
-
-
+plt.figure(figsize=(16, 8))
+plt.title("Distribution of Inverse document frequency (Top 50)")
+plt.xlabel("Frequency")
+plt.ylabel("Words")
+plt.bar(height=inverse_document_freq[:50]['idf'], 
+        x=inverse_document_freq[:50].index)
+plt.xticks(rotation=90)
+plt.show()
 
 
 
